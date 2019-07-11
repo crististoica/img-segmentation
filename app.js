587246d3794c145ede1ce0c4d50de9c15dbb4d71
cstoica <cristi6176@gmail.com>
2019-07-11 18:15:46 +0300

    Changed port

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const express = require('express');
 
 const app = express();
 
+const PORT = process.env.PORT || 3000;
+
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
-app.listen(3000);
+app.listen(PORT);
